48c8b21a324f87284e266a265d410ce403b3ab11
Christofer Nyström <nystromchristofer@gmail.com>
2026-08-27 09:48:09 +0200

    adds lexicon logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import logo from "./assets/lexicon-logo.svg";
+
+
 
 
 function App() {
 
   return (
     <>
+
+        <img
+            src={logo}
+            alt="Lexicon LMS"
+            className="w-40"
+        />
+
         <h1 className="text-4xl font-bold">
             Hello World!
         </h1>
+
     </>
   )
 }
