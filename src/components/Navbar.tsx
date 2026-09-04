f24bed0093e546ed3e94629fdf4bc5003c45b533
Sami Ahlfors <hello@samiahlfors.com>
2026-09-04 08:53:45 +0200

    style: add styling to navigation and header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,27 @@ import {currentUserContext} from "../context/currentUserContext.ts";
 import {useContext} from "react";
 
 export default function Navbar() {
-
     const user = useContext(currentUserContext);
-
-
     return (
-        <nav className="flex items-center border-b justify-around">
-            {user?.role === "teacher" ? (
-                <button>
-                    Webbutveckling ▾
-                </button>
-            ) : (
-                <span>
-                    Webbutveckling
-                </span>
-            )}
+        <nav className="w-full h-[48px] bg-white border-b border-slate-200 px-12 flex items-center justify-between text-xs font-medium text-slate-500">
+            <div className="w-full max-w-[1440px] px-12 flex items-center justify-between">
+                <div className="flex items-center gap-6">
+                    {user?.role === "teacher" ? (
+                    <button>
+                        Webbutveckling ▾
+                    </button>
+                    ) : (
+                        <span>
+                            Webbutveckling
+                        </span>
+                    )}
 
-            <NavLink to="/">Hem</NavLink>
-            <NavLink to="/modules">Moduler</NavLink>
-            <NavLink to="/schedule">Schema</NavLink>
-            <NavLink to="/submissions">Inlämningar</NavLink>
+                    <NavLink to="/">Hem</NavLink>
+                    <NavLink to="/modules">Moduler</NavLink>
+                    <NavLink to="/schedule">Schema</NavLink>
+                    <NavLink to="/submissions">Inlämningar</NavLink>
+                </div>
+            </div>
         </nav>
     );
 }
