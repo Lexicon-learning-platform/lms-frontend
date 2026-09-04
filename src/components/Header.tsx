35567be5159b00ae5136f13ee54f6c6f1fcab76a
Sami Ahlfors <hello@samiahlfors.com>
2026-09-04 13:33:01 +0200

    feat: center header and nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/lexicon-logo.svg";
 
 export default function Header() {
     return (
-        <header className="w-full h-[72px] bg-white border-b border-slate-200 px-12 flex items-center justify-between">
+        <header className="w-full h-[72px] bg-white border-b border-slate-200 flex justify-center">
             <div className="w-full max-w-[1440px] px-12 flex items-center justify-between">
                 <div className="flex items-center gap-3">
                     <img
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import {useContext} from "react";
 export default function Navbar() {
     const user = useContext(currentUserContext);
     return (
-        <nav className="w-full h-[48px] bg-white border-b border-slate-200 px-12 flex items-center justify-between text-xs font-medium text-slate-500">
+        <nav className="w-full h-[48px] bg-white border-b border-slate-200 flex justify-center text-xs font-medium text-slate-500">
             <div className="w-full max-w-[1440px] px-12 flex items-center justify-between">
                 <div className="flex items-center gap-6">
                     {user?.role === "teacher" ? (
