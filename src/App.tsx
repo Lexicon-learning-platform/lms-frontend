3e74ca37f4cef5b9169996a6a0bb3a6f5e4e4ed8
Christofer Nyström <nystromchristofer@gmail.com>
2026-09-12 22:16:38 +0200

    fix: hide navbar when logged out

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,26 @@ import Register from "./pages/Register.tsx";
 import Submissions from "./pages/Submissions.tsx";
 
 function App() {
-    const {user} = useCurrentUser();
+    const { user } = useCurrentUser();
 
     return (
         <div className="min-h-screen bg-slate-50 text-slate-800 flex flex-col items-center">
             <Header />
-            <Navbar />
+
+            {/* TODO: Decide whether logged-out users should have a navbar or not */}
+            {user &&
+                <Navbar />
+            }
 
             <main className="w-full max-w-[1680px] mx-auto flex-1 px-4 md:px-12 py-6 md:py-8 flex flex-col">
-
-            {user ? ( // todo change to auth later when fully implemented
-                    <>
-                        <Routes>
-                            <Route path="/" element={<PublicHome />} />
-                            <Route path="/home" element={<Home />} />
-                            <Route path="/modules" element={<Modules />} />
-                            <Route path="/schedule" element={<Schedule />} />
-                            <Route path="/submissions" element={<Submissions />} />
-                        </Routes>
-                    </>
+                {user ? (
+                    <Routes>
+                        <Route path="/" element={<PublicHome />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/modules" element={<Modules />} />
+                        <Route path="/schedule" element={<Schedule />} />
+                        <Route path="/submissions" element={<Submissions />} />
+                    </Routes>
                 ) : (
                     <Routes>
                         <Route path="/" element={<PublicHome />} />
