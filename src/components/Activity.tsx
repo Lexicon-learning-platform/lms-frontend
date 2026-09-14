d2a60c753bb2d553afe0caeacec91eceee30e96f
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-14 15:43:54 +0200

    reworking activity display

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
-import type { Activity } from "../models/activity.ts";
 import { useAuth } from "../context/auth/AuthContext.ts";
 import { authApiCall } from "../functions/authApiCall.ts";
+import type { ActivityExtended } from "../models/activityExtended.ts";
+import DialogBase from "./DialogBase.tsx";
+import Resource from "./Resource.tsx";
 
 interface ActivityContentProps {
     moduleId: string;
     activityId: string;
 }
 
-export default function Activity({ moduleId, activityId }: ActivityContentProps) {
+export default function Activity({
+    moduleId,
+    activityId,
+}: ActivityContentProps) {
     const { accessToken, setAccessToken } = useAuth();
-    const [detailedActivity, setDetailedActivity] = useState<Activity | null>(null);
+    const [detailedActivity, setDetailedActivity] =
+        useState<ActivityExtended | null>(null);
+    const [isResourceOpen, setIsResourceOpen] = useState<boolean>(false);
+    const [clickedResource, setClickedResource] = useState<string>("");
 
     useEffect(() => {
         async function loadActivity() {
             try {
-                const response = await authApiCall<Activity>(
+                const response = await authApiCall<ActivityExtended>(
                     `/modules/${moduleId}/activities/${activityId}`,
                     accessToken,
-                    setAccessToken
+                    setAccessToken,
                 );
 
                 console.log("activity response", response);
@@ -32,9 +40,56 @@ export default function Activity({ moduleId, activityId }: ActivityContentProps)
         loadActivity();
     }, [activityId, accessToken, setAccessToken, moduleId]);
 
+    if (!detailedActivity) return <p>Loading activity...</p>;
     return (
-        <pre className="whitespace-pre-wrap">
-            {JSON.stringify(detailedActivity, null, 4)}
-        </pre>
+        <div>
+            <div className="flex flex-col md:flex-row md:justify-between gap-4 mb-4">
+                <h2 className="text-2xl font-bold mb-8">
+                    {detailedActivity.name}
+                </h2>
+                <p className="text-nowrap">
+                    Längd: {detailedActivity.duration} dag
+                    {detailedActivity.duration > 1 ? "ar" : ""}
+                </p>
+            </div>
+            <p>{detailedActivity.description}</p>
+            <hr className="my-8" />
+            <div>
+                {detailedActivity.resources.length > 0 ? (
+                    detailedActivity.resources.map((r) => (
+                        <div
+                            key={r.id}
+                            className="border border-black rounded-lg p-1.5 mb-3"
+                            onClick={() => {
+                                setClickedResource(r.id);
+                                setIsResourceOpen(!isResourceOpen);
+                            }}
+                        >
+                            <h4 className="font-semibold">{r.name}</h4>
+                            <p>{r.description}</p>
+                            <p>Typ: {r.type}</p>
+                        </div>
+                    ))
+                ) : (
+                    <span>Inga resurser registrerade</span>
+                )}
+            </div>
+            {isResourceOpen && (
+                <DialogBase
+                    onClose={() => {
+                        setClickedResource("");
+                        setIsResourceOpen(false);
+                    }}
+                >
+                    <h3
+                        id="dialog-title"
+                        className="w-full text-center text-2xl mb-4"
+                    >
+                        Resursdetaljer
+                    </h3>
+                    <Resource resourceId={clickedResource} />
+                </DialogBase>
+            )}
+        </div>
     );
 }
