869f2fdef35f8a80afcdfda2619b9162f72485b7
Jonatan Streith <jonatan@streith.se>
2026-09-14 15:28:54 +0200

    All the user manipulation.

--- src/components/UserControl.tsx
+++ src/components/UserControl.tsx
@@ -2,8 +2,9 @@ import {useState, useEffect} from 'react'
 import type {ApplicationUser} from '../models/applicationUser.ts'
 import Button from './Button.tsx'
 import ErrorMessage from './Error.tsx'
-import {authApiCall} from '../functions/authApiCall'
+import {authApiCall} from '../functions/authApiCall.ts'
 import { useAuth } from "../context/auth/AuthContext.ts";
+import type { UserStats } from '../models/userStats.ts'
 
 
 
@@ -13,7 +14,8 @@ const UserList = () => {
     const [updateTrigger, setUpdateTrigger] = useState(0)
 
     const [users, setUsers] = useState<ApplicationUser[]>([])
-    const [selectedUser, setSelectedUser] = useState("")
+    const [selectedUser, setSelectedUser] = useState("")    //The Id of the selected user
+    const [activeUserStats, setActiveUserStats] = useState<UserStats>()
     const [error, setError] = useState<string>('');
         const { accessToken, setAccessToken } = useAuth();
 
@@ -21,6 +23,35 @@ const UserList = () => {
     const [newUserName, setNewUserName] = useState("");
     const [newUserPassword, setNewUserPassword] = useState("");
     const [newUserRole, setNewUserRole] = useState("");
+
+useEffect(() => {
+    let isMounted = true; 
+    const getUserStats = async () => {
+    try {
+        const data = await authApiCall<UserStats>(`/admin/getuserstats/${selectedUser}`,
+            accessToken,
+            setAccessToken,
+         { method: 'GET' });
+        if (isMounted && data) {
+          setActiveUserStats(data);
+        }
+    }catch (err: unknown) {
+        if (isMounted) {
+          if (err instanceof Error) {
+            setError(err.message);
+          } else {
+            setError('Ett okänt fel uppstod vid hämtning av uppgifter.');
+          }
+        }
+    } finally {
+      isMounted = false;
+    }
+    }
+
+getUserStats()
+}, [selectedUser]);
+
+
 
 useEffect(() => {
     let isMounted = true; 
@@ -38,7 +69,7 @@ useEffect(() => {
           if (err instanceof Error) {
             setError(err.message);
           } else {
-            setError('Ett okänt fel uppstod vid hämtning av recensioner.');
+            setError('Ett okänt fel uppstod vid hämtning av användare.');
           }
         }
     } finally {
@@ -48,7 +79,6 @@ useEffect(() => {
 
 fetchAllUsers()
 }, [updateTrigger]);
-
 
 
 const deleteUser = async () => {
@@ -111,8 +141,11 @@ const deleteUser = async () => {
 
 return (<>
 
-    <div id="alterUser" className="bg-slate-200 rounded-xl border border-slate-900 flex flex-col w-3/4 gap-6 p-6 m-6">
-    <select name="ApplicationUser" id="ApplicationUser" onChange={(e) => setSelectedUser(e.target.value)}>
+<div className='flex flex-row'>
+
+<aside className='flex flex-col w-1/4'>
+    <div id="alterUser" className="bg-slate-200 rounded-xl border border-slate-900 flex flex-col gap-6 p-6 m-6 h-auto">
+    <select name="UserList" onChange={(e) => setSelectedUser(e.target.value)}>
         <option value="">...</option>
         {users.map(user => (
 
@@ -121,20 +154,19 @@ return (<>
         ))}
 
     </select>
-
+<div className="flex gap-2 justify-between">
     <Button label="Radera" onClick={deleteUser} />
     <Button label="Deaktivera" onClick={disableUser} />
     <Button label="Reaktivera" onClick={enableUser} />
-
+</div>
     <ErrorMessage error={error} />
 
     </div>
 
-    <div id="registering" className="bg-slate-200 rounded-xl border border-slate-900 flex flex-col w-3/4 gap-6 p-6 m-6">
+    <div id="registering" className="bg-slate-200 rounded-xl border border-slate-900 flex flex-col gap-6 p-6 m-6">
     
-        <div className="mx-auto flex flex-col w-full bg-gray rounded-xl border border-slate-400 shadow-sm h-auto p-6 m-6">
-        <input className="w-auto bg-slate-100 border border-slate-700 rounded-lg p-2 focus:outline-none focus:border-emerald-500 m-1" type="text" value={newUserName} onChange={(e) => setNewUserName(e.target.value)} placeholder={"Title"} />
-        <input className="w-auto bg-slate-100 border border-slate-700 rounded-lg p-2 focus:outline-none focus:border-emerald-500 m-1" type="text" value={newUserPassword} onChange={(e) => setNewUserPassword(e.target.value)} placeholder={"Genre"} />
+        <input className="w-auto bg-slate-100 border border-slate-700 rounded-lg p-2 focus:outline-none focus:border-emerald-500 m-1" type="text" value={newUserName} onChange={(e) => setNewUserName(e.target.value)} placeholder={"Namn"} />
+        <input className="w-auto bg-slate-100 border border-slate-700 rounded-lg p-2 focus:outline-none focus:border-emerald-500 m-1" type="text" value={newUserPassword} onChange={(e) => setNewUserPassword(e.target.value)} placeholder={"Lösenord"} />
     
     <select className="w-auto bg-slate-100 border border-slate-700 rounded-lg p-2 focus:outline-none focus:border-emerald-500 m-1"
      onChange={(e) => setNewUserRole(e.target.value)}>
@@ -149,12 +181,52 @@ return (<>
     </div>
     
     <ErrorMessage error={error} />
-
     </div>
     
-    </div>
+    </aside>
 
 
+<main className='flex flex-col w-2/4 text-center'>
+        <div id="userdata" className="bg-slate-200 rounded-xl border border-slate-900 flex flex-col gap-6 p-6 m-6">
+            <h1>{activeUserStats?.userName || "Användarnamn"}</h1>
+            <h2>{activeUserStats?.givenName || "Förnamn"} {activeUserStats?.lastName || "Efternamn"}</h2>
+            <h2>{activeUserStats?.id || "Id"}</h2>
+
+<hr></hr>
+          <span>Kurser</span>
+          <table className="mx-auto table table-fixed table-hover">
+            <thead>
+                <tr>
+                    <th className='w-1/4'>Namn</th>
+                    <th className='w-1/4'>Startdatum</th>
+                    <th className='w-1/8'>Längd</th>
+
+                </tr>
+            </thead>
+
+            <tbody>
+                {activeUserStats?.courses?.map(course => (
+                
+               
+                <tr className="hover:bg-slate-200" key={course.id}>
+                    <td className='w-1/4'>{course.name}</td>
+                    <td className='w-1/4'>{course.startDate}</td>
+                    <td className='w-1/8'>{course.duration}</td>
+
+                </tr>
+                
+                ))}
+            </tbody>
+        </table>
+
+
+
+        </div>
+
+
+
+</main>
+</div>
 </>)
 
 }
