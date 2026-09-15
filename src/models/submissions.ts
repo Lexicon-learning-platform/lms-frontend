4e9def43b85cd7759ea10105f7c26d6c97014a07
Jonatan Streith <jonatan@streith.se>
2026-09-15 17:30:18 +0200

    Halfbaked submissions page

---
+++ src/models/submissions.ts
@@ -0,0 +1,4 @@
+export interface Submissions {
+    completedSubs: string[]
+    unCompletedSubs: string[]
+}
--- src/pages/Submissions.tsx
+++ src/pages/Submissions.tsx
@@ -1,9 +1,71 @@
-
+import {useState, useEffect} from 'react'
+import {authApiCall} from '../functions/authApiCall.ts'
+import { useAuth } from "../context/auth/AuthContext.ts";
+import ErrorMessage from "../components/Error.tsx";
+import type { Submissions } from '../models/submissions.ts';
+import {useCurrentUser} from "../context/currentUser/currentUserContext.ts";
+import {useCurrentCourse} from "../context/course/CourseContext.ts";
 
 export default function Submissions() {
+
+    const [subs, setSubs] = useState<Submissions>()
+    const [error, setError] = useState<string>('');
+    const { accessToken, setAccessToken } = useAuth();
+    const { user } = useCurrentUser();
+    const { course } = useCurrentCourse();
+
+
+useEffect(() => {
+    let isMounted = true; 
+    const fetchSubmissions = async () => {
+    try {
+        const data = await authApiCall<Submissions>(`/modules/${moduleId}/activities/${activityId}/completion/${user?.courseId}`,
+            accessToken,
+            setAccessToken,
+         { method: 'GET' });
+        if (isMounted && data) {
+          setSubs(data);
+        }
+    }catch (err: unknown) {
+        if (isMounted) {
+          if (err instanceof Error) {
+            setError(err.message);
+          } else {
+            setError('Ett okänt fel uppstod vid hämtning av användare.');
+          }
+        }
+    } finally {
+      isMounted = false;
+    }
+    }
+fetchSubmissions()
+}, []);
+
+
+
+
     return (
         <>
-            <p>???...</p>
+
+<div>
+         <h2>Kompletterade inlämningsuppgifter</h2>
+
+                {subs?.completedSubs?.map(comp => (
+                    <h3>{comp}</h3>                
+                ))}
+</div>
+
+<div>
+         <h2>Försenade inlämningsuppgifter</h2>
+
+                {subs?.unCompletedSubs?.map(comp => (
+                    <h3>{comp}</h3>                
+                ))}
+</div>
+
+               <ErrorMessage error={error} />
+ 
+
         </>
     );
 }
