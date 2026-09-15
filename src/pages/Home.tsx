3ec8b20d00e29a67c9a3af923888cecdd84c92c3
Christofer Nyström <nystromchristofer@gmail.com>
2026-09-15 15:59:20 +0200

    adds course data to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,56 @@
 import Card from "../components/Card";
 import Classmates from "../components/classmates";
+import { useCurrentCourse } from "../context/course/CourseContext.ts";
 
 export default function Home() {
+    const { course } = useCurrentCourse();
+
+    if (!course) {
+        return <p>Ingen kurs hittades.</p>;
+    }
+
     return (
         <>
-            <h1 className="text-xl font-bold">
-                Webbutveckling
-            </h1>
+            <section className="col-span-12 space-y-2">
+                <h1 className="text-xl font-bold text-slate-800">
+                    {course.name}
+                </h1>
 
-            <section className="col-span-12 space-y-6">
+                <p className="text-sm text-slate-600">
+                    {course.description}
+                </p>
+            </section>
+
+            <section className="col-span-12">
                 <Card>
-                    <h3 className="font-semibold text-slate-800 text-sm">⚠️ Ej inlämnad uppgift</h3>
-                    <p className="text-xs text-slate-500">Du har inte lämnat in uppgiften "React Components".</p>
+                    <div className="flex gap-8">
+                        <div>
+                            <p className="text-xs text-slate-500">
+                                Startdatum
+                            </p>
+                            <p className="font-semibold text-slate-800">
+                                {course.startDate}
+                            </p>
+                        </div>
+
+                        <div>
+                            <p className="text-xs text-slate-500">
+                                Kurslängd
+                            </p>
+                            <p className="font-semibold text-slate-800">
+                                {course.duration} veckor
+                            </p>
+                        </div>
+                    </div>
                 </Card>
             </section>
 
-            <section className="col-span-12 space-y-6">
-                <h2 className="mb-2 text-lg font-semibold">
-                    Pågående modul
-                </h2>
-
-                <div>
-                    <h3 className="font-semibold">
-                        React
-                    </h3>
-
-                    <p>24 aug – 18 sep</p>
-                </div>
-            </section>
 
             <section className="col-span-12 space-y-6">
-                <h2 className="mb-2 text-lg font-semibold">
-                    Nästa aktivitet
-                </h2>
-
-                <div>
-                    <h3 className="font-semibold">
-                        Föreläsning: React Router
-                    </h3>
-
-                    <p>Idag 10:00</p>
+                <div className="w-1/4">
+                    <Classmates />
                 </div>
             </section>
-
-           <section className="col-span-12 space-y-6">
-            <div className="w-1/4">
-            <Classmates  />
-            </div>
-           </section>
-
         </>
     );
 }
